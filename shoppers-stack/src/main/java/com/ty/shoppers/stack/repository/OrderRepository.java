788635d7bee6021502a07d9bd015d5c7eaef82d6
Adoni Meenakshi Naidu <118378129+meenakshi1237@users.noreply.github.com>
2024-02-27 10:27:59 +0530

    Delete shoppers-stack directory

--- shoppers-stack/src/main/java/com/ty/shoppers/stack/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ty.shoppers.stack.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ty.shoppers.stack.entity.Orders;
-
-public interface OrderRepository extends JpaRepository<Orders, Integer>{
-	
-}
